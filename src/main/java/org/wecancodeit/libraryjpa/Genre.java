79f515f18aa17bac7d81b73d94fc6cbfafbc10ee
marcbgold <marcbgold@yahoo.com>
2018-03-01 13:18:12 -0500

    adding genre entity

---
+++ src/main/java/org/wecancodeit/libraryjpa/Genre.java
@@ -0,0 +1,17 @@
+package org.wecancodeit.libraryjpa;
+
+public class Genre {
+
+	private String genre;
+
+	public Genre() {
+	}
+
+	public Genre(String genre) {
+		this.genre = genre;
+	}
+
+	public String getGenre() {
+		return genre;
+	}
+}
